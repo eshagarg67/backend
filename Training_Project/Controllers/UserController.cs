ac7afeaa49de5a6a21c6295d636073a22989b740
eshagarg67 <ishagarg675@gmail.com>
2019-11-01 17:24:46 +0530

    User changes

--- Training_Project/Controllers/UserController.cs
+++ Training_Project/Controllers/UserController.cs
@@ -1,10 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
 using System.Linq;
 using System.Net;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
 using Training_Project.Data;
 using Training_Project.Models;
 using Training_Project.Models.LocalModels;
@@ -59,7 +58,85 @@ namespace Training_Project.Controllers
                     result.Status = Status.Fail;
                     result.StatusCode = HttpStatusCode.BadRequest;
                     return result;
-                } 
+                }
+            }
+            catch (Exception e)
+            {
+                result.Status = Status.Error;
+                result.Message = e.Message;
+                result.StatusCode = HttpStatusCode.InternalServerError;
+                return result;
+            }
+        }
+
+        /// <summary>
+        /// Creates new user
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost("signup")]
+        [ProducesResponseType(typeof(bool), StatusCodes.Status206PartialContent)]
+        [ProducesResponseType(typeof(IResult), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public IResult CreateUser()
+        {
+            var result = new Result()
+            {
+                Operation = Operation.Create,
+                Status = Status.Success
+            };
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    result.Status = Status.Fail;
+                    result.StatusCode = HttpStatusCode.BadRequest;
+                    return result;
+                }
+                IFormFile img = null;
+                var userModel = JsonConvert.DeserializeObject<Users>(Request.Form["model"]);
+                var image = Request.Form.Files;
+                foreach (var i in image)
+                {
+                    img = image[0];
+                }
+                if (userModel != null)
+                {
+                    if (img == null)
+                    {
+                        result.Message = "User image does not exist";
+                        result.Status = Status.Fail;
+                        result.StatusCode = HttpStatusCode.BadRequest;
+                        return result;
+                    }
+                    var userDetailCheck = _context.Users.Where(u => (u.Username == userModel.Username) || (u.EmailId == userModel.EmailId)).FirstOrDefault();
+                    if (userDetailCheck != null)
+                    {
+                        if (userDetailCheck.Username == userModel.Username)
+                        {
+                            result.Message = "Username is already taken";
+                            result.Status = Status.Fail;
+                            result.StatusCode = HttpStatusCode.BadRequest;
+                            return result;
+                        }
+                        else
+                        {
+                            result.Message = "Email ID is already registered";
+                            result.Status = Status.Fail;
+                            result.StatusCode = HttpStatusCode.BadRequest;
+                            return result;
+                        }
+                    }
+                    ImageExtension imageExtension = new ImageExtension();
+                    userModel.UserImage = imageExtension.Image(img);
+                    _context.Users.Add(userModel);
+                    _context.SaveChangesAsync();
+
+                    return result;
+                }
+                result.Message = "Invalid user details entered";
+                result.Status = Status.Fail;
+                result.StatusCode = HttpStatusCode.BadRequest;
+                return result;
             }
             catch (Exception e)
             {
